ad76cf5348bc785d7aea034e264c5819f50dc3ff
VikashPR <vikashrajendran2806@gmail.com>
2022-08-22 15:19:49 +0530

    feat: data read from DB add

--- index/index.ino
+++ index/index.ino
@@ -1,9 +1,15 @@
 void setup() {
   // put your setup code here, to run once:
-
+Serial.begin(9600);
 }
-
+ int a = 0;
 void loop() {
   // put your main code here, to run repeatedly:
-
+  
+Serial.println(a);
+delay(1000);
+  a = a+1;
+  if(a >=100){
+    a = 0;
+  }
 }
